7b8ad4b9f429db0202adfd9c182b3176eb99c2b2
shreepatil424 <shreepatil424@gmail.com>
2021-04-24 21:55:06 +0530

    working on revert cmd

---
+++ abc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>new demo </title>
+</head>
+<body>
+	<p>new content added into abc file abc new hello bro how are you
+abc new  hi buddy im in feature pls merge me into master working with Unstaged file from staging area 
+working on remove last comit history
+</p> 
+</body>
+</html>
---
+++ demo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>new demo </title>
+</head>
+<body>
+	<p>new demo 
+	working on remove last comit history
+	</p> 
+</body>
+</html>
