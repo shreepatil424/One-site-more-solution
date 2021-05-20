d10fdad2bd822d1d0df040c6a095dcb07e5e01fa
shreepatil424 <shreepatil424@gmail.com>
2021-05-20 12:00:23 +0530

    updated demo file with new feature

--- demo.php
+++ demo.php
@@ -4,10 +4,13 @@
 	<title>new demo </title>
 </head>
 <body>
-	<p>new demo </p> 
-	<p>new XYZ</p> 
-	<p>new YSD </p> 
-	<p>new SDF </p> 
-	<p>new dprar </p> 
+ <h2>Iframe - Target for a Link</h2>
+
+<iframe src="demo_iframe.htm" name="iframe_a" height="300px" width="100%" title="Iframe Example"></iframe>
+
+<p><a href="https://www.w3schools.com" target="iframe_a">W3Schools.com</a></p>
+
+<p>When the target attribute of a link matches the name of an iframe, the link will open in the iframe.</p>
+
 </body>
 </html>	
