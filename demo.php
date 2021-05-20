67eff1345e5a0ecddc22bb4867408140bc46c1ea
shreepatil424 <shreepatil424@gmail.com>
2021-05-20 11:39:11 +0530

    new updates added in demo file

---
+++ demo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>new demo </title>
+</head>
+<body>
+	<p>new demo </p> 
+	<p>new XYZ</p> 
+	<p>new YSD </p> 
+	<p>new SDF </p> 
+	<p>new dprar </p> 
+</body>
+</html>	
